1983a0d8603304dce9f9d7537a60ff9a82dd37fb
Daniel Mai <danielmai1728@gmail.com>
2017-10-07 07:36:57 -0700

    Update changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDxA9yCsd_QbTC_ZP02KNm5mtdq8B0QRiE';
-const App = () => {
-  return (
-  <div>
-    <SearchBar />
-  </div>
-);
+YTSearch({key: API_KEY, term: 'surfboards'},  (videos) => {
+  this.setState({ videos });
+})
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { videos: [] };
+  }
+  render(){
+    return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+  }
+
 }
 ReactDOM.render(<App />, document.getElementById('root'));
