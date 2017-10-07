44f018935f38f2f0b4c0045321523bc514db913e
Daniel Mai <danielmai1728@gmail.com>
2017-10-06 20:49:01 -0700

    add youtube API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import SearchBar from './components/search_bar';
 
+const API_KEY = 'AIzaSyDxA9yCsd_QbTC_ZP02KNm5mtdq8B0QRiE';
+const App = () => {
+  return (
+  <div>
+    <SearchBar />
+  </div>
+);
+}
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
